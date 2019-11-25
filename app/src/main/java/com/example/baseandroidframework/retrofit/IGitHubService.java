fa249b983feba16f2baaec2500ddbb144d2b1470
aserbao <aserbao@163.com>
2019-11-25 09:05:29 +0800

    retrofit

--- app/src/main/java/com/example/baseandroidframework/retrofit/IGitHubService.java
+++ app/src/main/java/com/example/baseandroidframework/retrofit/IGitHubService.java
@@ -14,4 +14,11 @@ import retrofit2.http.Path;
 public interface IGitHubService {
     @GET("users/{user}/repos")
     Call<List<Repo>> listRepos(@Path("user") String user);
+
+    @GET("{user}/repos")
+    Call<List<Repo>> listRepos2(@Path("user") String user);
+
+
+    @GET("https://api.github.com/users/aserbao/repos")
+    Call<List<Repo>> listAbsRepos();
 }
--- app/src/main/java/com/example/baseandroidframework/retrofit/RetrofitActivity.java
+++ app/src/main/java/com/example/baseandroidframework/retrofit/RetrofitActivity.java
@@ -1,6 +1,7 @@
 package com.example.baseandroidframework.retrofit;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,14 +13,14 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import retrofit.GsonConverterFactory;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitActivity extends AppCompatActivity {
-
+    private static final String TAG = "RetrofitActivity";
 
     @BindView(R.id.show_tv)
     TextView mShowTv;
@@ -35,14 +36,16 @@ public class RetrofitActivity extends AppCompatActivity {
     @OnClick(R.id.request_btn)
     public void onViewClicked() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
+//                .baseUrl("https://api.github.com/")
+                .baseUrl("https://api.github.com/users/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         IGitHubService service = retrofit.create(IGitHubService.class);
 
-        Call<List<Repo>> listRepos = service.listRepos("aserbao");
-
+//        Call<List<Repo>> listRepos = service.listRepos("aserbao");
+        Call<List<Repo>> listRepos = service.listRepos2("aserbao");
+//        Call<List<Repo>> listRepos = service.listAbsRepos();
         listRepos.enqueue(new Callback<List<Repo>>() {
             @Override
             public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
@@ -54,14 +57,14 @@ public class RetrofitActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
     public void updateUi(List<Repo> body){
         StringBuffer stringBuffer = new StringBuffer();
         for (int i = 0; i < body.size(); i++) {
             Repo repo = body.get(i);
-            stringBuffer.append("第"+i+"个数据为：")
-                    .append(repo.toString())
+            stringBuffer.append(repo.getName())
                     .append("\n");
         }
         mShowTv.setText(stringBuffer.toString());
