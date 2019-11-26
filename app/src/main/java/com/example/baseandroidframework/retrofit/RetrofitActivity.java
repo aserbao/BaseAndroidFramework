b93f8eb0bb419b79e0555242a4012befe8e89480
aserbao <aserbao@163.com>
2019-11-26 09:38:09 +0800

    retrofit设置拦截器

--- app/src/main/java/com/example/baseandroidframework/retrofit/RetrofitActivity.java
+++ app/src/main/java/com/example/baseandroidframework/retrofit/RetrofitActivity.java
@@ -8,11 +8,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.baseandroidframework.R;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -35,16 +40,24 @@ public class RetrofitActivity extends AppCompatActivity {
 
     @OnClick(R.id.request_btn)
     public void onViewClicked() {
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().connectTimeout(15, TimeUnit.SECONDS)
+                //添加OkHttp3的拦截器
+                .addInterceptor(new LoggingInterceptor())
+                .writeTimeout(20, TimeUnit.SECONDS).readTimeout(20, TimeUnit.SECONDS)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
 //                .baseUrl("https://api.github.com/")
-                .baseUrl("https://api.github.com/users/")
+                .baseUrl("https://api.github.com/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
 
         IGitHubService service = retrofit.create(IGitHubService.class);
 
-//        Call<List<Repo>> listRepos = service.listRepos("aserbao");
-        Call<List<Repo>> listRepos = service.listRepos2("aserbao");
+        Call<List<Repo>> listRepos = service.listRepos("aserbao");
+//        Call<List<Repo>> listRepos = service.listRepos2("aserbao");
 //        Call<List<Repo>> listRepos = service.listAbsRepos();
         listRepos.enqueue(new Callback<List<Repo>>() {
             @Override
@@ -68,5 +81,21 @@ public class RetrofitActivity extends AppCompatActivity {
                     .append("\n");
         }
         mShowTv.setText(stringBuffer.toString());
+    }
+
+
+    class LoggingInterceptor implements Interceptor {
+        @Override public okhttp3.Response intercept(Chain chain) throws IOException {
+            Request request = chain.request();
+            long t1 = System.nanoTime();
+            Log.e(TAG, String.format("Sending request %s on %s%n%s",
+                    request.url(), chain.connection(), request.headers()));
+            okhttp3.Response response = chain.proceed(request);
+            long t2 = System.nanoTime();
+            Log.e(TAG, String.format("Received response for %s in %.1fms%n%s",
+                    response.request().url(), (t2 - t1) / 1e6d, response.headers()));
+
+            return response;
+        }
     }
 }
