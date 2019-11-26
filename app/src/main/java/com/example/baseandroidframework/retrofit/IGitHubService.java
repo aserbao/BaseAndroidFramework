b22920b0bee6d5f692264dfc632bccd373ca6341
aserbao <aserbao@163.com>
2019-11-26 21:24:53 +0800

    提交代码

--- app/src/main/java/com/example/baseandroidframework/retrofit/IGitHubService.java
+++ app/src/main/java/com/example/baseandroidframework/retrofit/IGitHubService.java
@@ -2,8 +2,13 @@ package com.example.baseandroidframework.retrofit;
 
 import java.util.List;
 
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.HTTP;
+import retrofit2.http.Multipart;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 /*
@@ -18,7 +23,11 @@ public interface IGitHubService {
     @GET("{user}/repos")
     Call<List<Repo>> listRepos2(@Path("user") String user);
 
+    @HTTP(method = "GET",path = "{user}/repos", hasBody = true)
+    Call<List<Repo>> listRepos3(@Path("user") String user);
 
     @GET("https://api.github.com/users/aserbao/repos")
     Call<List<Repo>> listAbsRepos();
+
+
 }
